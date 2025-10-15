14b8fd5bc6c22d63eb8ba3086214aa6e226ee361
That-weird-kid <zacxh.messi@gmail.com>
2025-10-14 20:38:11 -0600

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,11 +1,25 @@
+import time
 import random
-
-def produit(num):
+def func(num):
     list = []
     for i in range(6, 15):
         list.append(num * i)
-    print(random.choice(list))
-    #return list
+        #if i == 9:
+            #continue
+    answer = random.choice(list)
+    startt = time.time()
+    inputt = int(input(f"{num} x {int(answer / num)} = "))
+    endd = time.time()
+    timee = endd - startt
+    return answer, inputt, timee
+number = int(input("quelle nombre:"))
+while True:
+    answerr, inputtt, timee = func(number)
+    if inputtt == answerr:
+        print("good")
+        print(f"Ca pris {timee}")
+        
+    else:
+        print(f"bonne reponse {answerr}")
+        print(f"Ça pris {timee:.2f}")
 
-    
-print (produit(7))
