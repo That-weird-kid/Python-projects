b632183dffc3fa505d4eeea32e6559222bdecf5e
That-weird-kid <zacxh.messi@gmail.com>
2025-10-16 07:49:28 -0600

    Update prime number checking function
    Fix prime checking logic and improve output handling.

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,22 @@
 import random
+numDivisibles = []
+def func(number):
+    numDivisibles.clear()
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            numDivisibles.append(i)
+        if numDivisibles == []:
+            return False
+        return True
+        print(numdivisibles)
+a = random.choice(range(1,101))
+print(a)    
+inputt = input("is it a prime  y/n:")
+print(func(a))
+if inputt == n:
+    if a == False:
+        print("bravo")
+    else:
+        print(numDivisibles)
+if inputt:
+    
